71d7e371c7c40c5f1c4adc1145c503b71d200cf8
mozkoe <mozkoe@live.com>
2018-09-06 19:00:27 +0800

    add bottom popup actionsheet

---
+++ src/components/customActionSheet/customActionSheet.ts
@@ -0,0 +1,47 @@
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component({
+  name: 'CustomActionSheet',
+  mpType: 'component',
+})
+export default class CustomActionSheet extends Vue {
+  @Prop({ type: Number, default: 0 })
+  public offset!: number
+
+  public visible = false
+  public timer = 0
+  public classNames: Array<string> = []
+
+  public mounted() {
+    console.log(this)
+  }
+
+  public onUnload() {
+    this.visible = false
+  }
+
+  public open() {
+    clearTimeout(this.timer)
+    this.classNames = ['enter']
+    this.visible = true
+    this.timer = setTimeout(() => {
+      this.classNames = ['enter-to']
+      this.timer = setTimeout(() => {
+        this.classNames = []
+      }, 300)
+    })
+  }
+
+  public close() {
+    clearTimeout(this.timer)
+    this.classNames = ['leave']
+    this.visible = true
+    this.timer = setTimeout(() => {
+      this.classNames = ['leave-to']
+      this.timer = setTimeout(() => {
+        this.classNames = []
+        this.visible = false
+      }, 300)
+    })
+  }
+}
---
+++ src/components/customActionSheet/customActionSheet.vue
@@ -0,0 +1,67 @@
+<template>
+  <div
+    class="dialog"
+    :class="classNames"
+    :style="{ bottom: offset + 'rpx' }"
+    v-if="visible">
+    <div class="dialog-mask" @click="close"></div>
+    <div class="dialog-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script src="./customActionSheet.ts" lang="ts"></script>
+
+<style lang="less" scoped>
+  .dialog {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 100;
+    overflow: hidden;
+    transition: 0.3s;
+
+    &.enter-to, &.leave {
+      .dialog-mask {
+        background: rgba(0, 0, 0, 0.3);
+      }
+
+      .dialog-content {
+        transform: translateY(0);
+      }
+    }
+
+    &.leave-to, &.enter {
+      .dialog-mask {
+        background: rgba(0, 0, 0, 0);
+      }
+
+      .dialog-content {
+        transform: translateY(100%);
+      }
+    }
+
+    .dialog-mask {
+      position: relative;
+      z-index: 1;
+      height: 100%;
+      widows: 100%;
+      background: rgba(0, 0, 0, 0.3);
+      transition: 0.3s;
+    }
+
+    .dialog-content {
+      width: 100%;
+      background: white;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      transition: 0.3s;
+      transform: translateY(0);
+      z-index: 2;
+    }
+  }
+</style>
